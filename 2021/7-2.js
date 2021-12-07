068865592ebe7bf954b352e1f8454e7cc2b03309
bhaines <bhaines@yext.com>
2021-12-07 00:14:43 -0500

    day 7 answer

---
+++ 2021/7-2.js
@@ -0,0 +1,33 @@
+import { load } from "./helpers.js";
+import R from 'ramda';
+
+const score = (target, arr) => {
+	let sc = 0;
+	for (const n of arr) {
+		const diff = Math.abs(n - target);
+		sc += (diff * (diff + 1)) / 2;
+	}
+
+	return sc
+}
+
+const run = (arr) => {
+	const nums = arr[0].map(Number);
+	const min = Math.min(...nums);
+	const max = Math.max(...nums);
+
+	let bestScore = -1;
+	let t = null;
+	for (let target = min; target <= max; target++) {
+		const s = score(target, nums);
+		if (bestScore === -1 || s < bestScore) {
+			bestScore = s
+			t = target
+		}
+	}
+	console.log(bestScore);
+}
+
+load("7.txt")
+	.then(R.map(R.split(',')))
+	.then(run)
