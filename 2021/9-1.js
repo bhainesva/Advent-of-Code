0c6e17bf4ff8ce3f59969289a64260c95a76a463
bhaines <bhaines@yext.com>
2021-12-09 01:27:46 -0500

    weird refactor

--- 2021/9-1.js
+++ 2021/9-1.js
@@ -1,30 +1,24 @@
 import { load } from "./helpers.js";
 import R from 'ramda';
 
-const neighbs = (grid, r, c) => {
-	const out = [];
-	const dirs = [[-1,0], [1, 0], [0, -1], [0, 1]];
-	for (const d of dirs) {
-		const [dr, dc] = d;
-		const newr = r + dr;
-		const newc = c + dc;
-		if (newr < 0 || newr >= grid.length) continue;
-		if (newc < 0 || newc >= grid[0].length) continue;
-		out.push(grid[newr][newc]);
-	}
-	return out;
-}
+const addPoints = R.curry((a, b) => [a[0] + b[0], a[1] + b[1]]);
+const getCell = R.curry((grid, [r,c]) => grid[r][c]);
+const boundsCheck = R.curry((grid, [r, c]) => r >= 0 && c >= 0 && r < grid.length && c < grid[0].length);
+
+const neighbs = grid => R.compose(
+	R.map(getCell(grid)),
+	R.filter(boundsCheck(grid)),
+	R.flip(R.map)([[-1,0], [1, 0], [0, -1], [0, 1]]),
+	addPoints
+)
 
 const run = (arr) => {
-	const m = [];
-	for (const r of arr) {
-		m.push(r.split("").map(Number))
-	}
+	const m = R.map(R.compose(R.map(Number), R.split("")), arr)
 
 	let out = [];
 	for (let r = 0; r < m.length; r++) {
 		for (let c = 0; c < m[0].length; c++) {
-			const ns = neighbs(m, r, c);
+			const ns = neighbs(m)([r, c]);
 			const val = m[r][c];
 			if (val < Math.min(...ns)) {
 				out.push(val);
