e9d66978cd096ef311e88f5379b69fd871806d70
bhaines <bhaines@yext.com>
2021-12-10 13:38:27 -0500

    get those lines out of there

--- 2021/10-1.js
+++ 2021/10-1.js
@@ -5,7 +5,6 @@ const pair = {'(': ')', ')': '(', '[': ']', ']': '[', '{': '}', '}': '{', '<': '
 const pt = {')': 3, ']': 57, '}': 1197, '>': 25137};
 
 const red = (state, c) => {
-	if (state.invalid) return state;
 	if (['(', '{', '[', '<'].includes(c)) return {...state, stack: [...state.stack, pair[c]]}
 	if (R.last(state.stack) === c) return {...state, stack: R.init(state.stack)}
 	return R.reduced({...state, invalid: pt[c]});
@@ -13,7 +12,6 @@ const red = (state, c) => {
 
 load("10.txt")
 	.then(R.map(R.reduce(red, {stack: []})))
-	.then(R.filter(state => state.invalid))
-	.then(R.map(state => state.invalid))
+	.then(R.map(state => state.invalid || 0))
 	.then(R.sum)
 	.then(console.log);
